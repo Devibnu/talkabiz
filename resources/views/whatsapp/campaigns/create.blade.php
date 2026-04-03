6a3bed89a40c1f18bce1018e577a77e96205ed49
Nanang Ibnu Qosim <ibnuqosim@Nanangs-MacBook-Air.local>
2026-04-04 06:33:24 +0700

    Fix: WA Blast campaign views use correct auth layout

--- resources/views/whatsapp/campaigns/create.blade.php
+++ resources/views/whatsapp/campaigns/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.user_type.auth')
 
 @section('title', 'Buat Kampanye WA Blast')
 
--- resources/views/whatsapp/campaigns/show.blade.php
+++ resources/views/whatsapp/campaigns/show.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.user_type.auth')
 
 @section('title', 'Detail Kampanye - ' . $campaign->name)
 
