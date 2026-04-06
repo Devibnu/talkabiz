323ed51c43ccae147864f9aa5dcbc25a8dee01dd
Nanang Ibnu Qosim <ibnuqosim@Nanangs-MacBook-Air.local>
2026-04-06 21:44:46 +0700

    fix: mobile dashboard & billing provider autoDispose untuk refresh data saldo

--- mobile/talkabiz_mobile/lib/features/billing/presentation/providers/billing_provider.dart
+++ mobile/talkabiz_mobile/lib/features/billing/presentation/providers/billing_provider.dart
@@ -3,14 +3,14 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/repositories/billing_repository_impl.dart';
 import '../../domain/entities/billing_entities.dart';
 
-final billingOverviewProvider = FutureProvider<BillingOverview>((ref) async {
+final billingOverviewProvider = FutureProvider.autoDispose<BillingOverview>((ref) async {
   return ref.watch(billingRepositoryProvider).getOverview();
 });
 
-final billingPlansProvider = FutureProvider<List<PlanItem>>((ref) async {
+final billingPlansProvider = FutureProvider.autoDispose<List<PlanItem>>((ref) async {
   return ref.watch(billingRepositoryProvider).getPlans();
 });
 
-final topUpOptionsProvider = FutureProvider<List<TopUpOption>>((ref) async {
+final topUpOptionsProvider = FutureProvider.autoDispose<List<TopUpOption>>((ref) async {
   return ref.watch(billingRepositoryProvider).getTopUpOptions();
 });
--- mobile/talkabiz_mobile/lib/features/dashboard/presentation/providers/dashboard_provider.dart
+++ mobile/talkabiz_mobile/lib/features/dashboard/presentation/providers/dashboard_provider.dart
@@ -3,6 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../data/repositories/dashboard_repository_impl.dart';
 import '../../domain/entities/dashboard_summary.dart';
 
-final dashboardProvider = FutureProvider<DashboardSummary>((ref) async {
+final dashboardProvider = FutureProvider.autoDispose<DashboardSummary>((ref) async {
   return ref.watch(dashboardRepositoryProvider).getSummary();
 });
